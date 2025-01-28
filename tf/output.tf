ef3132b8f2b54a0c4dde13c4f9ebde5e2817553b
karishma-battina <67629745+karishma-battina@users.noreply.github.com>
2025-01-28 16:42:19 +0000

    comment output.tf

--- tf/output.tf
+++ tf/output.tf
@@ -18,8 +18,8 @@ output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
-resource "local_file" "kubeconfig" {
-  depends_on   = [azurerm_kubernetes_cluster.aks]
-  filename     = "kubeconfig"
-  content      = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
+# resource "local_file" "kubeconfig" {
+#   depends_on   = [azurerm_kubernetes_cluster.aks]
+#   filename     = "kubeconfig"
+#   content      = azurerm_kubernetes_cluster.aks.kube_config_raw
+# }
