df7ac04538bc2dff974035ceb53e5362d27393ab
Karishma Battina <67629745+karishma-battina@users.noreply.github.com>
2025-01-29 19:08:27 +0000

    comment data section

--- tf/main.tf
+++ tf/main.tf
@@ -82,18 +82,18 @@ resource "helm_release" "argocd" {
   }
 }
 
-data "kubernetes_secret" "argocd_admin" {
-  metadata {
-    name      = "argocd-initial-admin-secret"
-    namespace = "argocd"
-  }
-  depends_on = [helm_release.argocd]
-}
+#data "kubernetes_secret" "argocd_admin" {
+#  metadata {
+#    name      = "argocd-initial-admin-secret"
+#    namespace = "argocd"
+#  }
+#  depends_on = [helm_release.argocd]
+#}
 
-data "kubernetes_service" "argocd_server" {
-  metadata {
-    name      = "argocd-server" # Name of the service created by the Helm chart
-    namespace = "argocd"
-  }
-  depends_on = [helm_release.argocd]
-}
+#data "kubernetes_service" "argocd_server" {
+#  metadata {
+#    name      = "argocd-server" # Name of the service created by the Helm chart
+#    namespace = "argocd"
+#  }
+#  depends_on = [helm_release.argocd]
+#}
