b194a0511d0bca7ec251c7ce008bae3626d29c6b
Karishma Battina <67629745+karishma-battina@users.noreply.github.com>
2025-01-29 19:10:40 +0000

    Update output.tf

--- tf/output.tf
+++ tf/output.tf
@@ -24,11 +24,11 @@ output "acr_login_server" {
 #   content      = azurerm_kubernetes_cluster.argocd.kube_config_raw
 # }
 
-output "argocd_server_url" {
-  value = "http://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].ip}:80"
-}
+#output "argocd_server_url" {
+#  value = "http://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].ip}:80"
+#}
 
-output "argocd_admin_password" {
-  value     = data.kubernetes_secret.argocd_admin.data["password"]
-  sensitive = true
-}
+#output "argocd_admin_password" {
+#  value     = data.kubernetes_secret.argocd_admin.data["password"]
+#  sensitive = true
+#}
