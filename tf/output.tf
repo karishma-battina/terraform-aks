dbbe9c471e0107b44ed59e41dac1e5b6e3e8b53a
Karishma Battina <67629745+karishma-battina@users.noreply.github.com>
2025-01-31 02:05:06 +0000

    Remove duplicate vars

--- tf/output.tf
+++ tf/output.tf
@@ -23,23 +23,3 @@ output "acr_login_server" {
 #   filename     = "kubeconfig"
 #   content      = azurerm_kubernetes_cluster.argocd.kube_config_raw
 # }
-
-#output "argocd_server_url" {
-#  value = "http://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].ip}:80"
-#}
-
-#output "argocd_admin_password" {
-#  value     = data.kubernetes_secret.argocd_admin.data["password"]
-#  sensitive = true
-#}
-
-# Output the LoadBalancer IP
-output "argocd_loadbalancer_ip" {
-  value = kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].ip
-}
-
-# Output the ArgoCD admin password
-output "argocd_admin_password" {
-  value = kubernetes_secret.argocd_initial_admin_secret.data.password
-  sensitive = true
-}
